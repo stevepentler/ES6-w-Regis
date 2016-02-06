ca3b722dc28292a299d900b81c30e47740f79568
Steve Pentler <stevepentler@gmail.com>
2016-02-06 15:19:16 -0700

    Example class with calls

--- client.js
+++ client.js
@@ -13,3 +13,23 @@ $('#welcome').on('click', function (e) {
   console.log(this)
   console.log(e.toElement.textContent)
 })
+
+class Demo {
+  constructor(param1, param2) {
+    this.p1 = param1 || "bananas"
+    this.p2 = param2 || "bananas2"
+  }
+// get is like an attr_reader, you can call it without parenthesis
+  get outputParams() { 
+    console.log(`${this.p1} "AND" ${this.p2}`)
+  }
+
+  outputStuff() {
+    this.outputParams
+  }
+}
+
+const d = new Demo
+d.outputParams
+d.outputStuff()
+
